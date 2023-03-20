b12ffac9cd6bd487425042bfdc40a691a96c6cba
Lisa Knox <lisaknox83@gmail.com>
2023-03-20 15:49:57 -0400

    mobile nav ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,28 @@ export default function () {
     <header>
       <nav>
         {!Number.isNaN(pageNum) && pageNum > 0 ? (
-          <Link className='unstyled-link' to={pageNum ? `/${pageNum - 1}` : '/'}>
-            <div className='nav-button'>&lt; prev</div>
+          <Link
+            className='nav-button unstyled-link'
+            id='prev'
+            to={pageNum ? `/${pageNum - 1}` : '/'}
+          >
+            <div>&lt; prev</div>
           </Link>
         ) : (
           <div className='nav-button'></div>
         )}
-        <Link className='unstyled-link' to='/' id='home-button'>
-          <h1>Living the Program</h1>
-        </Link>
+        <div>
+          <Link className='unstyled-link' id='home-button' to='/'>
+            <h1>Living the Program</h1>
+          </Link>
+        </div>
         {!Number.isNaN(pageNum) && pageNum < 5 ? (
-          <Link className='unstyled-link' to={Number.isInteger(pageNum) ? `/${pageNum + 1}` : '/0'}>
-            <div className='nav-button right-text'>next &gt;</div>
+          <Link
+            className='nav-button unstyled-link'
+            id='next'
+            to={Number.isInteger(pageNum) ? `/${pageNum + 1}` : '/0'}
+          >
+            <div className='right-text'>next &gt;</div>
           </Link>
         ) : (
           <div className='nav-button'></div>

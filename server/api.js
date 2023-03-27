9d4c4b2ea40a279fa99c5798c11616b436e81ff8
Lisa Knox <lisaknox83@gmail.com>
2023-03-27 11:38:21 -0400

    made separate https server

--- server/api.js
+++ server/api.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 const path = require('path');
 
-// app.use('/', (req, res, next) => {
-//   console.log(req.hostname);
-//   if (req.hostname === 'localhost' || req.secure) {
-//     return next();
+// app.use('/public', express.static(path.join(__dirname, '../public')));
+// app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+// app.get('/', (req, res, next) => {
+//   try {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+//   } catch (error) {
+//     next(error);
 //   }
-//   res.redirect(301, `https://${req.headers.host}${req.url}`);
 // });
+
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(err.status || 500).send(err.message);
+// });
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('secrets/www_nadailyinventory_com.key', 'utf8');
+var certificate = fs.readFileSync('secrets/www_nadailyinventory_com.pem', 'utf8');
+
+var credentials = { key: privateKey, cert: certificate };
+var express = require('express');
+var app = express();
+
+// your express configuration here
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
-app.get('/', (req, res, next) => {
-  try {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  } catch (error) {
-    next(error);
-  }
-});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).send(err.message);
+// For http
+httpServer.listen(1953);
+// For https
+httpsServer.listen(2023);
+
+app.get('/', function (req, res) {
+  res.header('Content-type', 'text/html');
+  return res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;

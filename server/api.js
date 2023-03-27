0438df03dfe15c3251046afdbd2bc85f60bdbdd3
Lisa Knox <lisaknox83@gmail.com>
2023-03-27 12:49:15 -0400

    Revert "httpssssssssssss"
    This reverts commit 15e6038f48d30075c013b382adc627ce755e2b5f.

--- server/api.js
+++ server/api.js
@@ -13,38 +13,17 @@ const path = require('path');
 //   }
 // });
 
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(err.status || 500).send(err.message);
-// });
+app.get('/', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  } catch (error) {
+    next(error);
+  }
+});
 
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-const dotenv = require('dotenv');
-dotenv.config();
-var privateKey = process.env.EC_PRIVATE_KEY;
-var certificate = process.env.EC_PRIVATE_CERT;
-var credentials = { key: privateKey, cert: certificate };
-var express = require('express');
-var app = express();
-
-// your express configuration here
-
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-// For http
-httpServer.listen(1953);
-// For https
-httpsServer.listen(2023);
-
-app.get('/', function (req, res) {
-  res.header('Content-type', 'text/html');
-  return res.sendFile(path.join(__dirname, '../public/index.html'));
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).send(err.message);
 });
 
 module.exports = app;

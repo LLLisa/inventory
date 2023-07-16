d82620d594267fc56051491b4fa8329eae7c3ae9
Lisa Knox <lisaknox83@gmail.com>
2023-07-16 17:20:08 -0400

    fixed scroll restore

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,24 +1,25 @@
 import React, { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { Header, Footer } from '.';
 
 export default () => {
-  useEffect(() => {
-    window.addEventListener('beforeunload', alertUser);
-    return () => {
-      window.removeEventListener('beforeunload', alertUser);
+    useEffect(() => {
+        window.addEventListener('beforeunload', alertUser);
+        return () => {
+            window.removeEventListener('beforeunload', alertUser);
+        };
+    }, []);
+    const alertUser = (e) => {
+        e.preventDefault();
+        e.returnValue = '';
     };
-  }, []);
-  const alertUser = (e) => {
-    e.preventDefault();
-    e.returnValue = '';
-  };
 
-  return (
-    <div className='main-container'>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='main-container'>
+            <Header />
+            <Outlet />
+            <Footer />
+            <ScrollRestoration />
+        </div>
+    );
 };

cf3e29c08c837f4fe65df6e451579ea7c6e4343a
Lisa Knox <lisaknox83@gmail.com>
2023-09-02 15:04:58 -0400

    styling

--- src/components/views/InstallPWA.js
+++ src/components/views/InstallPWA.js
@@ -7,17 +7,14 @@ export default () => {
                 <h2 className="center-text">Install this app on your phone</h2>
                 <article id="install-text">
                     <b>Step 1:</b>
-                    <ul>
-                        <li>
-                            iPhone: Open <a href="https://nadailyinventory.com">nadailyinventory.com</a> in Safari and tap the share button (the
-                            square with an up arrow) at the bottom of the screen to open the share menu.
-                        </li>
-                        <br />
-                        <li>
-                            Android: Open <a href="https://nadailyinventory.com">nadailyinventory.com</a> in Chrome and tap the menu button (three
-                            dots) in the top right corner.
-                        </li>
-                    </ul>
+                    <p>
+                        <b>iPhone:</b> Open <a href="https://nadailyinventory.com">nadailyinventory.com</a> in Safari and tap the share button (the
+                        square with an up arrow) at the bottom of the screen to open the share menu.
+                    </p>
+                    <p>
+                        <b>Android:</b> Open <a href="https://nadailyinventory.com">nadailyinventory.com</a> in Chrome and tap the menu button (three
+                        dots) in the top right corner.
+                    </p>
                     <b>Step 2:</b>
                     <p>Scroll down and tap "Add to Home Screen."</p>
                     <b>Step 3:</b>
